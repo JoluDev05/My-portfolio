e40fa1a736e9ae398c3d111c8c7faa42b0886f0d
Jorge Luis Arce <jorgearcedev1@gmail.com>
2025-12-17 23:06:39 -0700

    Created service section

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ import { Hero } from "@/components/Hero";
 import { Stats } from "@/components/Stats";
 import { Projects } from "@/components/Projects";
 import { About } from "@/components/About";
+import { Services } from "@/components/Services";
 
 export const App = () => {
   return (
@@ -23,6 +24,7 @@ export const App = () => {
       <Stats/>
       <Projects/>
       <About/>
+      <Services/>
     </main>
   );
 };
